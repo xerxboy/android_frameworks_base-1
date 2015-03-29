2183d41ece4b24aa831e2e6ffee09965f107954f
maxwen <max.weninger@gmail.com>
2015-03-29 18:39:55 +0200

    base: allow disable of screenshot shutter sound [1/2]
    Change-Id: I47d52bba21170118af87d35376d81d7569587a2f
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4944,6 +4944,13 @@ public final class Settings {
         public static final String DOUBLE_TAP_SLEEP_GESTURE = "double_tap_sleep_gesture";
 
 
+         /**
+          * Screenshod sound enable, This is the noise made when taking a screesnhot
+          * Defaults to 1 - sounds enabled
+          * @hide
+          */
+         public static final String SCREENSHOT_SHUTTER_SOUND = "screenshot_shutter_sound";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
