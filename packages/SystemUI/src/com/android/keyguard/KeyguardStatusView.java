5f7b8f516b443ae254d996e4339b1863543d7207
Sarthak Aditya <sarthak.aditya52@gmail.com>
2019-05-12 22:01:01 +0000

    Q clock fixes
    Signed-off-by: sayan7848 <sayan7848@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
@@ -502,6 +502,7 @@ public class KeyguardStatusView extends GridLayout implements
                        View.GONE) : View.VISIBLE);
                 mKeyguardSlice.setVisibility(mShowInfo ? View.VISIBLE : View.GONE);
                 mCustomClockView.setVisibility(View.GONE);
+                mDuClockView.setVisibility(View.GONE);
                 mTextClock.setVisibility(View.GONE);
                 break;
             case 7: // custom text clock
