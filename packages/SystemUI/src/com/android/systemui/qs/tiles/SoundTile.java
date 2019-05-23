001fbcdbe3fe8a96b5f895359d4c8df10208a12f
dracarys18 <karuhegde4343@gmail.com>
2019-05-23 04:54:26 +0000

    Adapt DU

--- packages/SystemUI/src/com/android/systemui/qs/tiles/SoundTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/SoundTile.java
@@ -142,6 +142,6 @@ public class SoundTile extends QSTileImpl<BooleanState> {
 
     @Override
     public int getMetricsCategory() {
-return MetricsEvent.RESURRECTED;
+return MetricsEvent.DIRTYTWEAKS;
     }
 }
