7d72641c6bbab538c360182f8b259e21fa30e521
dracarys18 <karthihegde010@gmail.com>
2019-08-05 15:29:45 +0000

    Revert "Add sammy accent clock and a variant [1/2]"
    This reverts commit 41eb393ad94531887df6a055459a219763c6a57f.

--- packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
@@ -59,7 +59,6 @@ import com.android.systemui.util.wakelock.KeepAwakeAnimationListener;
 
 import com.google.android.collect.Sets;
 
-import java.lang.Math;
 import java.util.Locale;
 
 public class KeyguardStatusView extends GridLayout implements
@@ -406,12 +405,6 @@ public class KeyguardStatusView extends GridLayout implements
         } else if (mClockSelection == 5) {
             mClockView.setFormat12Hour(Html.fromHtml("<strong>hh</strong><br>mm"));
             mClockView.setFormat24Hour(Html.fromHtml("<strong>kk</strong><br>mm"));
-        } else if (mClockSelection == 5) {
-            mClockView.setFormat12Hour(Html.fromHtml("hh<br><font color=" + getResources().getColor(R.color.sammy_minutes_accent) + ">mm</font>"));
-            mClockView.setFormat24Hour(Html.fromHtml("kk<br><font color=" + getResources().getColor(R.color.sammy_minutes_accent) + ">mm</font>"));
-        } else if (mClockSelection == 6) {
-            mClockView.setFormat12Hour(Html.fromHtml("<font color='#454545'>hh</font><br><font color=" + getResources().getColor(R.color.sammy_minutes_accent) + ">mm</font>"));
-            mClockView.setFormat24Hour(Html.fromHtml("<font color='#454545'>kk</font><br><font color=" + getResources().getColor(R.color.sammy_minutes_accent) + ">mm</font>"));
         } else {
             mClockView.setFormat12Hour("hh\nmm");
             mClockView.setFormat24Hour("kk\nmm");
@@ -626,11 +619,6 @@ public class KeyguardStatusView extends GridLayout implements
                 mDuClockView.setVisibility(View.GONE);
 		mKeyguardSlice.setVisibility(mShowInfo ? View.VISIBLE : View.GONE);
                 mCustomClockView.setVisibility(View.GONE);
-            case 6: // sammy accent
-                mClockView.setVisibility(mDarkAmount != 1 ? (mShowClock ? View.VISIBLE :
-                       View.GONE) : View.VISIBLE);
-                mCustomClockView.setVisibility(View.GONE);
-                break;
         }
     }
 
@@ -683,12 +671,6 @@ public class KeyguardStatusView extends GridLayout implements
                  params.addRule(RelativeLayout.BELOW, R.id.custom_textclock_view);
 	 	 updateTextClockAlign();
                  break;
-            case 6: // sammy accent
-                params.addRule(RelativeLayout.BELOW, R.id.clock_view);
-                mClockView.setSingleLine(false);
-                mClockView.setGravity(Gravity.CENTER);
-                mClockView.setLineSpacing(0, 0.8f);
-                break;
         }
 
         updateVisibilities();
